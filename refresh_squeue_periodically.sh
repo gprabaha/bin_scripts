f74f6d92c8ef17c508a62ba8ab7b1cd770dc8764
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-05-31 12:32:54 -0400

    adding scripts

---
+++ refresh_squeue_periodically.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Loop indefinitely
+while true; do
+    clear  # Clear the terminal window
+    echo "Job Progress:"
+    squeue -u pg496 # Display job queue status
+    sleep 30  # Wait for 10 seconds before checking again
+done
+
